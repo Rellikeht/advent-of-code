d67cdb92df0579cf57c93e2cacbbee1b52fef9bf
Rellikeht <michaelhemperek@gmail.com>
2024-07-02 17:45:28 +0200

    got the most evil aoc solution working

--- aoc2021/day4/sol1.sh
+++ aoc2021/day4/sol1.sh
@@ -1,39 +1,78 @@
-#!/usr/bin/env dash
 #!/usr/bin/env sh
+#!/usr/bin/env dash
 
-read ns
-boards=0
+# numbers to guess
+read -r ns
+
+# board number
+board=0
+
+# directory for commiting this crime
 DIR="$(mktemp -d)"
 cd "$DIR"
 
-while read ignored; do
-    mkdir $boards
-    cd $boards
+while read -r _; do
+    mkdir $board
 
+    # go to current board
+    cd $board
+
+    # dirs for columns
     for i in $(seq 5); do
         mkdir "c$i"
     done
 
+    # for each row
     for i in $(seq 5); do
         mkdir "r$i"
 
-        read row
-        row="$(echo $row | sed -E 's/^ *//;s/ +/\n/g')"
+        # get numbers in a row
+        read -r row
+
+        # change format to loop friendly
+        row="$(echo "$row" | sed -E 's/^ *//;s/ +/\n/g')"
+
+        # because seq starts with 1
         j=1
-        for f in $(echo $row); do
+
+        for f in $row; do
+
+            # create file for each number
             touch "r$i/$f"
             touch "c$j/$f"
+
             j=$((j + 1))
         done
     done
 
     cd ..
-    boards=$((boards + 1))
+    board=$((board + 1))
 done
 
-# for i in $(echo $ns | sed 's/,/\n/g'); do
-#     echo $i
-# done
+# for all numbers to guess
+for i in $(echo "$ns" | sed 's/,/\n/g'); do
 
-# echo "$DIR"
+    # remove number from all boards
+    find . -type f -a -name "$i" -exec rm {} \;
+
+    # check if there is board with all numbers marked
+    find . -type d -empty | grep -E '.*' >/dev/null && break
+done
+
+find . -type d -empty | # getting all boards with bingo
+    while read -r d; do
+        d="${d%/*}"
+        echo "${d#./}"
+    done |    # getting directory names
+    sort -r | # highest number
+    sed 1q |  # only one
+    xargs -I{} \
+        find {} -type f \
+        -wholename '*r*' \
+        -printf '%f + ' | # adding numbers
+    sed 'i0 ' |           # for expression to be correct
+    sed "\$a$i * p\0" |   # multiplying by last guessed number
+    xargs -d'\0' dc -e    # calculating score
+
+# finally
 rm -r "$DIR"
