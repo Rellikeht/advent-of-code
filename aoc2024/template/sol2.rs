e6f2a05f3b652c084d641c6adb81cedc75faa42c
Rellikeht <michaelhemperek@gmail.com>
2024-12-26 17:39:55 +0100

    templates update

--- aoc2024/template/sol2.rs
+++ aoc2024/template/sol2.rs
@@ -1,6 +1,21 @@
-use std::io;
+use std::fs::File;
+use std::io::BufRead;
+use std::{env, io};
 
 pub fn main() {
-    for ln in io::stdin().lines() {}
+    let args: Vec<String> = env::args().collect();
+    let fname: &str;
+    if args.len() > 1 {
+        fname = &args[1];
+    } else {
+        fname = "input";
+    }
+    let file = File::open(fname).unwrap();
+    let mut lines = io::BufReader::new(file).lines();
+
+    for ln in lines {
+        //
+    }
+
     println!("{}", 0);
 }
